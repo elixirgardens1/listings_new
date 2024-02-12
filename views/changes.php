19b1da5fc70b777f5e1781a4708aacc01843d988
David <daveswaves.2008@gmail.com>
2024-02-12 09:55:12 +0000

    Update views/changes.php to delete records older than 40 days.

--- views/changes.php
+++ views/changes.php
@@ -20,10 +20,12 @@ $files_used[] = 'views/changes.php'; //DEBUG
 <div class="h60"></div>
 
 <?php
-// Delete records from changes table older than 28 days
-// $timestamp_minus_1_month = strtotime("-28 days");
-// $sql = "DELETE FROM `changes` WHERE `timestamp` < $timestamp_minus_1_month";
-// $db_listings->query($sql);
+$db_listings->beginTransaction();
+// Delete records from changes table older than 40 days
+$timestamp_minus_1_month = strtotime("-40 days");
+$sql = "DELETE FROM `changes` WHERE `timestamp` < $timestamp_minus_1_month";
+$db_listings->query($sql);
+$db_listings->commit();
 
 // Need users name
 $sql = "SELECT id,name FROM `user`";
